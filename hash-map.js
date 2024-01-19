12fd4581aa3fced28579850bca4f29f1203cdf53
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-19 03:25:26 +0100

    add bucket growth logic

--- hash-map.js
+++ hash-map.js
@@ -1,16 +1,18 @@
 class HashMap {
-  #mod = 16;
+  #bucketSize = 16;
+  #entriesCount = 0;
   #store = [];
+  #loadFactor = 0.75;
 
   constructor(size = 16) {
-    this.#mod = size;
+    this.#bucketSize = size;
     this.#fillStore();
   }
 
   #fillStore() {
     this.#store = [];
 
-    for (let i = 0; i < this.#mod; i += 1) {
+    for (let i = 0; i < this.#bucketSize; i += 1) {
       this.#store.push(null);
     }
   }
@@ -23,18 +25,37 @@ class HashMap {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
 
-    return hashCode % this.#mod;
+    return hashCode % this.#bucketSize;
+  }
+
+  #growBucket() {
+    const existingNodesArr = this.entries();
+    this.#store = [];
+    this.#bucketSize = this.#bucketSize * 2;
+    this.#fillStore();
+
+    for (let node of existingNodesArr) {
+      const [key, value] = node;
+      this.set(key, value);
+    }
   }
 
   set(key, value) {
     const hashedIndex = this.#hash(key);
+
+    if ((this.#entriesCount / this.#bucketSize) >= this.#loadFactor) {
+      this.#growBucket();
+    }
 
     if (this.#store[hashedIndex] === null) {
       this.#store[hashedIndex] = {
         key,
         value,
       };
+      this.#entriesCount += 1;
     } else {
+      console.log(this.#store[hashedIndex]);
+      console.log(key, value)
       if (this.#store[hashedIndex].key === key) {
         this.#store[hashedIndex].value = value;
       }
@@ -131,17 +152,27 @@ class HashMap {
 const hashMap = new HashMap();
 hashMap.set('firstKey', 'firstValue');
 hashMap.set('secondKey', 'secondValue');
-console.log(hashMap.entries());
-console.log(hashMap.values());
+hashMap.set("game2", "Monopoly");
+hashMap.set("car2", "Ford");
+hashMap.set("constellation2", "Ursa Major");
+hashMap.set("school2", "Harvard");
+hashMap.set("country2", "Australia");
+hashMap.set("continent2", "Antarctica");
+hashMap.set("job3", "Doctor");
+hashMap.set("music3", "Hip Hop");
+hashMap.set("drink4", "Coca Cola");
+hashMap.set("animal3", "Kangaroo");
+hashMap.set("book3", "1984");
+hashMap.set("city4", "Tokyo");
+hashMap.set("food3", "Pasta");
+hashMap.set("emotion3", "Excited");
+hashMap.set("element3", "Platinum");
+hashMap.set("team3", "Red Sox");
+hashMap.set("movie3", "The Godfather");
+hashMap.set("subject4", "Chemistry");
+hashMap.set("color3", "Purple");
+hashMap.set("language3", "Java");
+// console.log(hashMap.entries());
+// console.log(hashMap.values());
 console.log(hashMap.keys());
 console.log(hashMap.length);
-console.log(hashMap.remove('secondKey'));
-console.log(hashMap.get('firstKey'));
-console.log(hashMap.has('firstKey'));
-hashMap.clear();
-console.log(hashMap.has('firstKey'));
-console.log(hashMap.entries());
-console.log(hashMap.values());
-console.log(hashMap.keys());
-console.log(hashMap.length);
-console.log(hashMap.remove('secondKey'));
