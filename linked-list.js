b8dea523ad72a08a03d4ac212c3577665768a510
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-20 05:31:49 +0100

    remove unused methods from class

--- linked-list.js
+++ linked-list.js
@@ -4,22 +4,6 @@ module.exports = class LinkedList {
   #size = 0;
   #head = null;
 
-  append(key, value) {
-    if (this.#head === null) {
-      this.#head = new Node(key, value);
-    } else {
-      let tail = this.#head;
-
-      while (tail.nextNode !== null) {
-        tail = tail.nextNode;
-      }
-
-      tail.nextNode = new Node(key, value);
-    }
-
-    this.#size += 1;
-  }
-
   prepend(key, value) {
     if (this.#head === null) {
       this.#head = new Node(key, value);
@@ -40,23 +24,11 @@ module.exports = class LinkedList {
     return this.#head;
   }
 
-  get tail() {
-    if (this.isEmpty()) return null;
-
-    let tail = this.#head;
-
-    while (tail.nextNode !== null) {
-      tail = tail.nextNode;
-    }
-
-    return tail;
-  }
-
   isEmpty() {
     return this.#size === 0;
   }
 
-  at(index) {
+  #at(index) {
     if (index < 0 || index >= this.#size) return null;
 
     if (this.isEmpty()) return null;
@@ -70,45 +42,6 @@ module.exports = class LinkedList {
     }
 
     return node;
-  }
-
-  pop() {
-    if (this.isEmpty()) return undefined;
-
-    let tail = this.#head;
-
-    if (tail.nextNode === null) {
-      this.#head = null;
-      return tail;
-    }
-
-    let beforeTail = null;
-
-    while (tail.nextNode !== null) {
-      beforeTail = tail;
-      tail = tail.nextNode;
-    }
-
-    beforeTail.nextNode = null;
-    this.#size -= 1;
-
-    return tail;
-  }
-
-  contains(value) {
-    if (this.isEmpty()) return false;
-
-    let currentHead = this.#head;
-
-    if (currentHead.value === value) return true;
-
-    while (currentHead.nextNode !== null) {
-      let next = currentHead.nextNode;
-      if (next.value === value) return true;
-      currentHead = next;
-    }
-
-    return false;
   }
 
   hasKey(key) {
@@ -223,39 +156,6 @@ module.exports = class LinkedList {
     return entriesArr;
   }
 
-  toString() {
-    let node = this.#head;
-    let output = ``;
-
-    while (node !== null) {
-      output += `[Key: ${node.key} - Value: ${node.value}] -> `;
-      node = node.nextNode;
-    }
-
-    return (output += 'null');
-  }
-
-  insertAt(value, index) {
-    if (index < 0) {
-      // Invalid index, cannot insert
-      return null;
-    }
-
-    if (index === 0) {
-      this.prepend(value);
-    } else if (index >= this.#size) {
-      this.append(value);
-    } else {
-      const newNode = new Node(value);
-      const previousNode = this.at(index - 1);
-
-      newNode.nextNode = previousNode.nextNode;
-      previousNode.nextNode = newNode;
-    }
-
-    this.#size += 1;
-  }
-
   removeAt(index) {
     if (index < 0 || index >= this.#size) return false;
 
@@ -268,8 +168,8 @@ module.exports = class LinkedList {
         this.#head.nextNode = this.#head.nextNode.nextNode;
       }
     } else {
-      let occupyingNode = this.at(index);
-      let prevNode = this.at(index - 1);
+      let occupyingNode = this.#at(index);
+      let prevNode = this.#at(index - 1);
 
       if (occupyingNode !== null && prevNode !== null) {
         prevNode.nextNode = occupyingNode.nextNode;
